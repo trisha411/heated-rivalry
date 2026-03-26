94473dd4a3de961ec4a5d87d685172b9008675e3
Trisha Sharan <trishasharan11@gmail.com>
2026-03-26 14:40:40 -0400

    first commit

---
+++ aggregateData.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+# HEAT VULNERABILITY INDEX
+hvi <- read_csv("/Users/trisha/courses/applied_ml/data/Heat_Vulnerability_Index_Rankings_20260326.csv")
+
+# POPULATION DENSITY (2020 CENSUS)
+density <- read_csv("/Users/trisha/courses/applied_ml/data/us_population_density_by_zip_september2020.csv")
+
+# NATURAL GAS USAGE 2010
+nat_gas <- read_csv("/Users/trisha/courses/applied_ml/data/Natural_Gas_Consumption_by_ZIP_Code_-_2010_20260326.csv")
+
+# STEAM USAGE 2010
+steam <- read_csv("/Users/trisha/courses/applied_ml/data/Steam_Consumption_by_ZIP_Code_-_2010_20260326.csv")
+
+# TEMPERATURE
